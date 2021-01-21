3897a8e4f90abd5ddeafb1e7e7f9339768333c17
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-01-20 17:10:18 -0800

    Try configuring first, then installing

--- cmake-modules/az_vcpkg.cmake
+++ cmake-modules/az_vcpkg.cmake
@@ -53,10 +53,15 @@ macro(az_vcpkg_export targetName macroNamePart dllImportExportHeaderPath)
     set(AZ_${macroNamePart}_DLL_INSTALLED_AS_PACKAGE "*/ + 1 /*")
     configure_file(
       "${CMAKE_CURRENT_SOURCE_DIR}/inc/${dllImportExportHeaderPath}"
-      "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}/${dllImportExportHeaderPath}"
+      "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_INCLUDEDIR}/${dllImportExportHeaderPath}"
       @ONLY
     )
     unset(AZ_${macroNamePart}_DLL_INSTALLED_AS_PACKAGE)
+
+    install(
+        FILES "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_INCLUDEDIR}/${dllImportExportHeaderPath}"
+        DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/${dllImportExportHeaderPath}"
+    )
   endif()
 
   # Export the targets file itself.
